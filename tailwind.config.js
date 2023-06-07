8a5c4670da09371a715bab274dedf1419b32faa9
Shivam Goyal <shivam@192.168.1.5>
2023-06-07 11:58:30 +0530

    Mobile website completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.html"],
+  content: ["./dist/*.html", "./dist/js/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "open-menu": {
+          "0%": { transform: "scaleY(0)" },
+          "80%": { transform: "scaleY(1.2)" },
+          "100%": { transform: "scaleY(0)" },
+        },
+        animation: {
+          "open-menu": "open-menu 0.5s ease-in-out forwards",
+        },
+      },
+    },
   },
   plugins: [],
 };
